739dcb954961ebbb31e7b1337e015bd959d23cd7
Jack "Zilla" Lyons <zilla@Mac-Zilla-7.local>
2026-05-15 16:01:41 +0900

    Let Hostinger manage server hostname

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ if (process.env.NODE_PORT && !process.env.PORT) {
   process.env.PORT = process.env.NODE_PORT;
 }
 
-if (!process.env.HOSTNAME || process.env.HOSTNAME.startsWith("/") || process.env.HOSTNAME.includes("extapp-sock")) {
-  process.env.HOSTNAME = "0.0.0.0";
-}
-
 const standaloneServer = path.join(__dirname, ".next", "standalone", "server.js");
 
 console.log(
